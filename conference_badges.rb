42584bf8699ce8787724da3727a3a4c150f40330
andrewbay <daddnbay@gmail.com>
2021-01-14 13:17:00 -0800

    commit tree ce504b3e3b9a6cb070d4572a0955ff1394fd0ad1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    greeting = []
+    names.each do |names|
+        greeting << badge_maker(names)
+    end
+    return greeting
+end
+
+def assign_rooms(speakers)
+    greet = []
+    speakers.each_with_index do |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
+    end
+    return greet
+end
+
+def printer(attendees)
+    result = batch_badge_creator(attendees)
+  result.each do |x|
+    puts x 
+  end
+  nextresult = assign_rooms(attendees)
+  nextresult.each do |x|
+    puts x 
+  end
+end
